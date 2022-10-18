de9adafd6c4b5d8c5e185183837ba0ba24bc1f6f
rishonkumar <rishon.kumar.rahi@gmail.com>
2022-10-18 09:34:06 +0530

    SDE

--- src/SDE/Arrays_1/Next_Permutation.java
+++ src/SDE/Arrays_1/Next_Permutation.java
@@ -1,2 +1,44 @@
-package SDE.Arrays_1;public class Next_Permutation {
+package SDE.Arrays_1;
+
+public class Next_Permutation {
+    /*
+    Algo :
+     traverse from back n - 2
+     find first idx (break point) a[i] >= a[i+1] => i1
+
+     travese from n-1 find the number from right exactly smallest greater element than i1 element and swap
+
+     reverse (i1+1,last)
+
+    edge case if no break point then just reverse
+*/
+    public void nextPermutation(int[] A) {
+
+        int n = A.length;
+
+        if (A == null || n == 0) return;
+
+        int i = n - 2;
+        while (i >= 0 && A[i] >= A[i + 1]) i--;
+
+        //found the break point
+        if (i >= 0) {
+            int j = n - 1;
+            while (A[i] >= A[j]) j--;
+            swap(A, i, j);
+        }
+        reverse(A, i + 1, n - 1);
+    }
+
+    void swap(int[] A, int i, int j) {
+        int t = A[i];
+        A[i] = A[j];
+        A[j] = t;
+    }
+
+    void reverse(int[] A, int i, int j) {
+        while (i < j) {
+            swap(A, i++, j--);
+        }
+    }
 }
--- src/SDE/Arrays_1/Pascal_Traingle.java
+++ src/SDE/Arrays_1/Pascal_Traingle.java
@@ -1,2 +1,26 @@
-package SDE.Arrays_1;public class Pascal_Traingle {
+package SDE.Arrays_1;
+//https://leetcode.com/problems/pascals-triangle/
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pascal_Traingle {
+
+    public List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < i + 1; j++) {
+                // 1st and last are 1
+                if (j == 0 || j == i) row.add(1);
+//                arr[i][j] = arr[i-1][j] + arr[j-1][j];
+                else {
+                    row.add(res.get(i - 1).get(j - 1) + res.get(i - 1).get(j));
+                }
+            }
+            res.add(row);
+        }
+        return res;
+    }
 }
